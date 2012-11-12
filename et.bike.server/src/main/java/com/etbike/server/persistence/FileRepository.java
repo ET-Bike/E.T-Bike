1f06f6d9f7c704b20d59f519a2b745381087d81f
TaeUng <yukult400@gmail.com>
2012-11-13 04:19:29 +0900

    file download URL 필드 추가

--- et.bike.server/src/main/java/com/etbike/server/persistence/FileRepository.java
+++ et.bike.server/src/main/java/com/etbike/server/persistence/FileRepository.java
@@ -1,9 +1,11 @@
 package com.etbike.server.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import com.etbike.server.domain.model.Board;
 import com.etbike.server.domain.model.UploadedFile;
 
-public interface FileRepository extends JpaRepository<UploadedFile, Long> {
+public interface FileRepository extends JpaRepository<UploadedFile, Long> ,JpaSpecificationExecutor<UploadedFile>{
 
 }
---
+++ et.bike.server/src/main/java/com/etbike/server/persistence/FileSpecifications.java
@@ -0,0 +1,33 @@
+package com.etbike.server.persistence;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.etbike.server.domain.model.UploadedFile_;
+import com.etbike.server.domain.model.UploadedFile;
+
+public class FileSpecifications {
+
+
+	public static Specification<UploadedFile> isfileName(final String searchTerm){
+		return new Specification<UploadedFile>(){
+			@Override
+			public Predicate toPredicate(Root<UploadedFile> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				return cb.like(root.<String>get(UploadedFile_.fileName), getLikePattern(searchTerm));
+			}
+		};
+	}
+
+
+	private static String getLikePattern(final String searchTerm) {
+		StringBuilder pattern = new StringBuilder();
+		pattern.append("%");
+		pattern.append(searchTerm);
+		pattern.append("%");
+		return pattern.toString();
+	}
+}
