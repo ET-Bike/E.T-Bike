a671b42b7c4acdb60a13212895a50153162461ea
TaeUng <yukult400@gmail.com>
2012-11-24 21:13:08 +0900

    PushingController 추가

--- et.bike.server/src/main/java/com/etbike/server/web/controller/BoardRepoController.java
+++ et.bike.server/src/main/java/com/etbike/server/web/controller/BoardRepoController.java
@@ -34,6 +34,18 @@ public class BoardRepoController {
 	@Autowired private AccountRepository accountRepository;
 	@Autowired private FileRepository fileRepository;
 	
+	@RequestMapping(value="/request/push")
+	@ResponseBody
+	public String requestPush(@PathVariable ModelMap map){
+
+		
+		//sendMsg("doo871128","응답바");
+		//map.put("myBikeList", myBikeList);
+		//eturn "jsonView";
+		return "OK";
+		
+	}
+	
 	@RequestMapping(value="/shareBoard/getMyBikeList/{username}")
 	public String getMyDealList(@PathVariable String username, ModelMap map){
 		MyBikeList myBikeList = new MyBikeList();
